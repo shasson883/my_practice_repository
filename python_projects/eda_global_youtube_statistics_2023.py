785a5f08c044881d7be22a0fe3fef1d3438eabb5
Stephen Hasson <shasson883@googlemail.com>
2023-08-22 17:04:37 +0100

    20230822_latest updates to python projects

--- python_projects/eda_global_youtube_statistics_2023.py
+++ python_projects/eda_global_youtube_statistics_2023.py
@@ -13,6 +13,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from wordcloud import WordCloud
+import folium
 
 
 # Import csv dataset & convert to pandas dataframe (df)
@@ -198,6 +200,8 @@ df.to_csv(r'C:\Users\shass\my_github\my_practice_repository\datasets\global_yout
 # Apply plot style 'seaborn' for visualisations                  
 plt.style.use('seaborn')
 
+
+
 ## Correlation matrix heatmap
 
 # Adjust the default figure size for matplotlib plots
@@ -241,6 +245,7 @@ plt.yticks(fontsize = 15)
 plt.tight_layout()
 
 
+
 ## Which are the top 10 channel categories by no. YouTube channels?
 
 # Calculate the number of YouTube channels in each category
@@ -250,6 +255,7 @@ print(category_counts_asc)
 # Select the top 10 categories based on the number of YouTuber channels
 top_10_categories_asc = category_counts_asc.tail(10)
 print(top_10_categories_asc)
+
 
 # Plot df as a horizontal bar chart
 top_10_categories_asc.plot.barh(x = 'No. YouTube Channels', y = 'Category', stacked = True, color = '#FF0000', width = 0.75)
@@ -261,6 +267,8 @@ plt.ylabel('Channel Category', fontsize = 16, weight = 'bold')
 plt.xticks(fontsize = 15)
 plt.yticks(fontsize = 15)
 plt.tight_layout()
+
+
 
 ## Which countries have the highest number of YouTubers?
 
@@ -287,19 +295,17 @@ colors += [(0.8, 0.8, 0.8)]
 
 # create a pie chart using Matplotlib with adjusted start angle and larger font size
 wedges, texts, autotexts = plt.pie(data, labels = labels, autopct = '%1.0f%%', colors = colors, startangle = 150, textprops = {'fontsize': 15})
-
 # equal aspect ratio ensures that pie is drawn as a circle
 plt.axis('equal')
-
 # increase the font size of the pie chart labels and percentage values
 for text in texts:
     text.set_fontsize(15)
 for autotext in autotexts:
     autotext.set_fontsize(15)
-
 # Adjust chart formatting 
 plt.title('% Distribution of YouTube Channels by Country (2023)', fontsize = 25, weight = 'bold')
 plt.tight_layout()
+
 
 
 ## Is there a correlation between the number of subscribers and the number of views?
@@ -307,34 +313,53 @@ plt.tight_layout()
 # Create new variable for expressing values in millions
 df_mns = df
 
-# Reflect 'Subscriber' & 'Video Views' in millions
+# Create new columns to support visualisation
 df_mns['Subscribers (mn.)'] = (df_mns['Subscribers'] / 1000000).astype(int)
 df_mns['Video Views (bn.)'] = (df_mns['Video Views'] / 1000000000).astype(int)
+df_mns['Uploads (k.)'] = (df_mns['Uploads'] / 1000)
 
 # Create new df for scatter plot
 df_sub_pop = df_mns[['Subscribers (mn.)', 'Video Views (bn.)']]
 df_sub_pop
 
+# Calculate the correlation coefficient
+correlation = df_sub_pop['Subscribers (mn.)'].corr(df_sub_pop['Video Views (bn.)'])
+
 # Create linear regression for trend line
 m, b = np.polyfit(df_sub_pop['Subscribers (mn.)'], df_sub_pop['Video Views (bn.)'], 1)
+
+# Set scaling factor of circle size
+scaling_factor = 4
+
+# Calculate the size of circles 
+sizes = df_sub_pop['Subscribers (mn.)'] * scaling_factor
 
 # Set figuresize
 plt.figure(figsize=(16, 12))
 
 # Plot df as a scatter
-plt.scatter(df_sub_pop['Subscribers (mn.)'], df_sub_pop['Video Views (bn.)'], color='#FF0000', alpha=0.6)
+plt.scatter(df_sub_pop['Subscribers (mn.)'], df_sub_pop['Video Views (bn.)'], s = sizes, color = '#FF0000', alpha=0.6)
 
 # Plot trendline
-plt.plot(df_sub_pop['Subscribers (mn.)'], m * df['Subscribers (mn.)'] + b, color='#FF0000')
+plt.plot(df_sub_pop['Subscribers (mn.)'], m * df_sub_pop['Subscribers (mn.)'] + b, color='#FF0000')
 
 # Adjust chart formatting 
-plt.title('Correlation Between Subscribers & Video Views (2023)', fontsize = 25, weight = 'bold')
-plt.xlabel('Number of Subscribers', fontsize = 16,  weight = 'bold')
-plt.ylabel('Number of Video Views', fontsize = 16,  weight = 'bold')
+plt.title('Correlation Between No. Subscribers & No. Video Views (2023)', fontsize = 25, weight = 'bold')
+plt.xlabel('No. Subscribers (mn.)', fontsize = 16,  weight = 'bold')
+plt.ylabel('No. Video Views (mn.)', fontsize = 16,  weight = 'bold')
 plt.xticks(fontsize = 15)
 plt.yticks(fontsize = 15)
 plt.grid(True, which = 'both', linestyle = '--', linewidth = 0.5)
+
+plt.annotate(f'Correlation: {correlation:.2f}', 
+             xy = (0.85, 0.025), 
+             xycoords = 'axes fraction', 
+             fontsize = 15, 
+             weight = 'bold')
+
 plt.show()
+
+# Strong positive correlation
 
 """
 np.polyfit(): This is a function from the numpy library. It's used to fit a polynomial (of a specified degree) 
@@ -358,23 +383,302 @@ In essence, this line of code is using a linear regression to find the best-fitt
 """
 
 
-
-
 ## Is there a correlation between the number of subscribers and the number of videos?
+
+# Create new variable for expressing values in millions
+df_mns = df
+
+# Create new df for scatter plot
+df_sub_pop_2 = df_mns[['Subscribers (mn.)', 'Uploads (k.)']]
+df_sub_pop_2
+
+# Calculate the correlation coefficient
+correlation = df_sub_pop_2['Subscribers (mn.)'].corr(df_sub_pop_2['Uploads (k.)'])
+
+# Create linear regression for trend line
+m, b = np.polyfit(df_sub_pop_2['Subscribers (mn.)'], df_sub_pop_2['Uploads (k.)'], 1)
+
+# Set scaling factor of circle size
+scaling_factor = 4 
+
+# Calculate the size of circles 
+sizes = df_sub_pop_2['Subscribers (mn.)'] * scaling_factor
+
+# Set figuresize
+plt.figure(figsize = (16, 12))
+
+# Plot df as a scatter
+plt.scatter(df_sub_pop_2['Subscribers (mn.)'], df_sub_pop_2['Uploads (k.)'], s = sizes, color = '#FF0000', alpha = 0.6)
+
+# Plot trendline
+plt.plot(df_sub_pop_2['Subscribers (mn.)'], m * df_sub_pop_2['Subscribers (mn.)'] + b, color = '#FF0000')
+
+# Adjust chart formatting 
+plt.title('Correlation Between No. Subscribers & No. Video Uploads (2023)', fontsize = 25, weight = 'bold')
+plt.xlabel('No. Subscribers (mn.)', fontsize = 16, weight = 'bold')
+plt.ylabel('No. Video Uploads (k.)', fontsize = 16, weight = 'bold')
+plt.xticks(fontsize = 15)
+plt.yticks(fontsize = 15)
+plt.grid(True, which = 'both', linestyle = '--', linewidth = 0.5)
+
+plt.annotate(f'Correlation: {correlation:.2f}', 
+             xy = (0.85, 0.025), 
+             xycoords = 'axes fraction', 
+             fontsize = 15, 
+             weight = 'bold')
+
+plt.show()
+
+# Weak positive correlation
+
+
 
 ## Are certain categories more correlated with high view or subscriber counts?
 
-## What is the average view-to-subscriber ratio across different categories?
+# Create new df to group by category & aggregate columns
+category_sum = df.groupby('Category').agg({
+    'Subscribers (mn.)': 'sum',
+    'Video Views (bn.)': 'sum'
+}).reset_index()
 
+# Validate results of new df
+print(category_sum)
+
+# Sort categories by no. Subscribers (mn.)
+category_sum_subs = category_sum.sort_values(by = 'Subscribers (mn.)', ascending = False)
+category_sum_subs
+
+# Sort categories by no. Video Views (bn.)
+category_sum_views = category_sum.sort_values(by = 'Video Views (bn.)', ascending = False)
+category_sum_views
+
+# Plot df as a bar chart
+ax = category_sum_subs.plot(kind = 'bar', stacked = True, x = 'Category', y = 'Subscribers (mn.)', color = '#FF0000')
+
+# Add data labels above the bars
+for bar in ax.containers:
+    ax.bar_label(bar, label_type='edge', color='black', fontsize = 15, padding = 5)
+
+# Adjusting the chart formatting
+plt.title('No. Subscribers by Channel Category (2023)', fontsize = 25, weight = 'bold')
+plt.xlabel('Channel Category', fontsize = 16,  weight = 'bold')
+plt.ylabel('Subscribers (mn.)', fontsize = 16, weight = 'bold')
+plt.xticks(fontsize = 15)
+plt.yticks(fontsize = 15)
+plt.tight_layout()
+
+# Create new df for bar chart, Category vs Video Views (bn.)
+
+# Plot df as a bar chart
+ax = category_sum_views.plot(kind = 'bar', stacked = True, x = 'Category', y = 'Video Views (bn.)', color = '#FF0000')
+
+# Add data labels above the bars
+for bar in ax.containers:
+    ax.bar_label(bar, label_type='edge', color='black', fontsize = 15, padding = 5)
+
+# Adjusting the chart formatting
+plt.title('No. Video Views by Channel Category (2023)', fontsize = 25, weight = 'bold')
+plt.xlabel('Channel Category', fontsize = 16,  weight = 'bold')
+plt.ylabel('Video Views (bn.)', fontsize = 16, weight = 'bold')
+plt.xticks(fontsize = 15)
+plt.yticks(fontsize = 15)
+plt.tight_layout()
+
+
+
+## What is the average view-to-subscriber ratio across different categories?
 ## Which categories have the most engaged audiences, as measured by views per subscriber?
+
+# Create new dataframe using raw values
+category_sum_raw = df.groupby('Category').agg({
+    'Subscribers': 'sum',
+    'Video Views': 'sum'
+}).reset_index()
+
+# Create new variable to calculate the 'Subscriber to View Ratio'
+category_sum_raw['View-to-Subscriber Ratio'] = category_sum_raw['Video Views'] / category_sum_raw['Subscribers']
+category_sum_raw['View-to-Subscriber Ratio'] = category_sum_raw['View-to-Subscriber Ratio'].astype(float)
+
+# Sort df by 'Subscriber to View Ratio' descending
+category_sum_raw = category_sum_raw.sort_values(by = 'View-to-Subscriber Ratio', ascending = False)
+
+# Plot df as a bar chart
+ax = category_sum_raw.plot(kind = 'bar', stacked = True, x = 'Category', y = 'View-to-Subscriber Ratio', color = '#FF0000')
+
+# Add data labels above the bars
+for bar in ax.containers:
+    ax.bar_label(bar, label_type='edge', labels=[f'{int(val)}' for val in bar.datavalues], color='black', fontsize=15, padding=5)
+
+# Adjusting the chart formatting
+plt.title('Avg. View-to-Subscriber Ratio by Channel Category (2023)', fontsize = 25, weight = 'bold')
+plt.xlabel('Channel Category', fontsize = 16,  weight = 'bold')
+plt.ylabel('Avg. View-to-Subscriber Ratio', fontsize = 16, weight = 'bold')
+plt.xticks(fontsize = 15)
+plt.yticks(fontsize = 15)
+plt.tight_layout()
+
+"""
+Explained - the 'View-to-Subscriber Ratio' show indicates on avg. how many views are correlated per additional Subscriber
+e.g. for every additional subscriber gained on a Pets & Animals category you can expect to get on average 619 additional views
+across videos
+
+"""
+
 
 ## What are the most common words in high-view channel descriptions?
 
-## Which categories are potentially the most lucrative, based on views and subscriber counts?
+# Determine the 75th percentile of video views
+percentile_75th = df['Video Views'].quantile(0.75)
+
+# Filter channels with views above the 75th percentile
+high_view_channels = df[df['Video Views'] > percentile_75th]
+
+# Combine the names of high-view channels into a single string
+text = ' '.join(high_view_channels['Youtuber'])
+
+# Create the word cloud
+wordcloud = WordCloud(background_color = 'white', width = 800, height = 400).generate(text)
+
+# Plot the word cloud
+plt.imshow(wordcloud, interpolation = 'bilinear')
+plt.axis('off')
+plt.title('Word Cloud of High-View YouTube Channel Descriptions', fontsize = 25, weight = 'bold')
+plt.show()
+
+
+
+## Which categories are potentially the most lucrative based on avg. monthly earnings
+
+# Calculate avg. monthly averages per category rounded to 0 decimal places
+df['Avg. Monthly Earnings'] = (df['Lowest Monthly Earnings'] + df['Highest Monthly Earnings']) / 2
+
+# Create new df to group by category & calculate mean avg. monthly earnings by category
+category_avg_earnings = df.groupby('Category').agg({
+    'Avg. Monthly Earnings': 'mean',
+    }).reset_index()
+
+# Create new column to reflect avg. monthly earnings in thousands, rounded down to nearest whole number 
+category_avg_earnings['Avg. Monthly Earnings ($000s)'] = (category_avg_earnings['Avg. Monthly Earnings'] / 1000).astype(int)
+category_avg_earnings
+
+# Sort categories by no. Subscribers (mn.)
+category_avg_earnings = category_avg_earnings.sort_values(by = 'Avg. Monthly Earnings ($000s)', ascending = False)
+
+# Plot df as a bar chart
+ax = category_avg_earnings.plot(kind = 'bar', stacked = True, x = 'Category', y = 'Avg. Monthly Earnings ($000s)', color = '#FF0000')
+
+# Add data labels above the bars
+for bar in ax.containers:
+    ax.bar_label(bar, label_type = 'edge', color = 'black', fontsize = 15, padding = 5)
+
+# Adjusting the chart formatting
+plt.title('Avg. Monthly Earnings by Channel Category (2023)', fontsize = 25, weight = 'bold')
+plt.xlabel('Channel Category', fontsize = 16,  weight = 'bold')
+plt.ylabel('Avg. Monthly Earnings ($000s)', fontsize = 16, weight = 'bold')
+plt.xticks(fontsize = 15)
+plt.yticks(fontsize = 15)
+plt.tight_layout()
+
+
 
 ## Is there a correlation between the number of views and estimated revenue?
 
+df_sub_pop_4 = df[['Video Views', 'Avg. Monthly Earnings']].copy()
+
+df_sub_pop_4['Video Views (bn.)'] = df_sub_pop_4['Video Views'] / 1000000000
+df_sub_pop_4['Avg. Monthly Earnings ($000s)'] = df_sub_pop_4['Avg. Monthly Earnings'] / 1000
+
+
+# Calculate the correlation coefficient
+correlation = df_sub_pop_4['Video Views (bn.)'].corr(df_sub_pop_4['Avg. Monthly Earnings ($000s)'])
+
+# Create linear regression for trend line
+m, b = np.polyfit(df_sub_pop_4['Video Views (bn.)'], df_sub_pop_4['Avg. Monthly Earnings ($000s)'], 1)
+
+# Sort data by 'Video Views (bn.)'
+df_sub_pop_4 = df_sub_pop_4.sort_values(by='Video Views (bn.)')
+
+# Set scaling factor of circle size
+scaling_factor = 4 
+
+# Set figuresize
+plt.figure(figsize = (16, 12))
+
+# Calculate the size of circles 
+sizes = df_sub_pop_4['Video Views (bn.)'] * scaling_factor
+
+# Plot df as a scatter
+plt.scatter(df_sub_pop_4['Video Views (bn.)'], df_sub_pop_4['Avg. Monthly Earnings ($000s)'], color = '#FF0000', alpha = 0.6)
+
+# Plot trendline
+plt.plot(df_sub_pop_4['Video Views (bn.)'], m * df_sub_pop_4['Video Views (bn.)'] + b, color = '#FF0000')
+
+# Adjust chart formatting 
+plt.title('Correlation Between Video Views & Avg. Monthly Earnings (2023)', fontsize = 25, weight = 'bold')
+plt.xlabel('Video Views (bn.)', fontsize = 16, weight = 'bold')
+plt.ylabel('Avg. Monthly Earnings ($000s)', fontsize = 16, weight = 'bold')
+plt.xticks(fontsize = 15)
+plt.yticks(fontsize = 15)
+plt.grid(True, which = 'both', linestyle = '--', linewidth = 0.5)
+
+plt.annotate(f'Correlation: {correlation:.2f}', 
+             xy = (0.85, 0.025), 
+             xycoords = 'axes fraction', 
+             fontsize = 15, 
+             weight = 'bold')
+
+plt.show()
+
+# Moderate positive correlation
  
+"""
+
+## Geographic map
+
+# Calculate the number of YouTuber channels in each country
+country_counts = df['Country'].value_counts()
+
+# Convert the series to a df
+country_counts = country_counts.reset_index()
+
+# Create column headers
+country_counts.columns = ['Country', 'Youtuber Count']
+
+# Validate new df
+country_counts
+
+world_map = folium.Map(location = [20,0], tiles = "cartodbpositron", zoom_start = 2)
+
+world_map.choropleth(
+    geo_data = 'https://raw.githubusercontent.com/python-visualization/folium/master/examples/data/world-countries.json',
+    name = 'choropleth',
+    data = country_counts,
+    columns = ['Country', 'Youtuber Count'],
+    key_on = 'feature.properties.name',
+    fill_color = 'YlOrRd', 
+    fill_opacity = 0.7, 
+    line_opacity = 0.2,
+    legend_name = 'Country Counts'
+)
+
+folium.LayerControl().add_to(world_map)
+
+world_map.save('world_map.html')
+
+world_map
+
+"""
+
+
+"""
+df_country = df.groupby('Country').agg({
+    'Youtuber': 'sum',
+    'Subscribers': 'sum',
+    'Video Views': 'sum',
+}).reset_index()
+"""
+
+
 
 
 

82000923b916705d94003b834d759daf41ac5a32
Stephen Hasson <shasson883@googlemail.com>
2023-09-13 11:09:28 +0100

    20230913_latest updates

--- python_code/useful_python_code.py
+++ python_code/useful_python_code.py
@@ -9,68 +9,74 @@ Created on Fri Sep  1 10:46:42 2023
 DataCamp - Python Data Science Toolbox (Part 1)
 """
 
-### Return a dictionary with counts of occurrences as value for each key
+# =============================================================================
+# Return a dictionary with counts of occurrences as value for each key
+# =============================================================================
 
-# # Define count_entries()
-# def count_entries(df, *args):
-#     """Return a dictionary with counts of occurrences as value for each key."""
+# Define count_entries()
+def count_entries(df, *args):
+    """Return a dictionary with counts of occurrences as value for each key."""
     
-#     #Initialize an empty dictionary: cols_count
-#     cols_count = {}
+    #Initialize an empty dictionary: cols_count
+    cols_count = {}
     
-#     # Iterate over column names in args
-#     for col_name in args:
+    # Iterate over column names in args
+    for col_name in args:
     
-#         # Extract column from DataFrame: col
-#         col = df[col_name]
+        # Extract column from DataFrame: col
+        col = df[col_name]
     
-#         # Iterate over the column in DataFrame
-#         for entry in col:
+        # Iterate over the column in DataFrame
+        for entry in col:
     
-#             # If entry is in cols_count, add 1
-#             if entry in cols_count.keys():
-#                 cols_count[entry] += 1
+            # If entry is in cols_count, add 1
+            if entry in cols_count.keys():
+                cols_count[entry] += 1
     
-#             # Else add the entry to cols_count, set the value to 1
-#             else:
-#                 cols_count[entry] = 1
+            # Else add the entry to cols_count, set the value to 1
+            else:
+                cols_count[entry] = 1
 
-#     # Return the cols_count dictionary
-#     return cols_count
+    # Return the cols_count dictionary
+    return cols_count
 
-# # Call count_entries(): result1
-# result1 = count_entries(tweets_df, 'lang')
+# Call count_entries(): result1
+result1 = count_entries(tweets_df, 'lang')
 
-# # Call count_entries(): result2
-# result2 = count_entries(tweets_df, 'lang', 'source')
+# Call count_entries(): result2
+result2 = count_entries(tweets_df, 'lang', 'source')
 
-# # Print result1 and result2
-# print(result1)
-# print(result2)
+# Print result1 and result2
+print(result1)
+print(result2)
 
 
-# """
+
+# =============================================================================
 # DataCamp - Python Data Science Toolbox (Part 1)
-# """
+# =============================================================================
 
-# ### Create list from filter object
 
-# # Select retweets from the Twitter DataFrame: result
-# result = filter(lambda x: x[0:2] == 'RT', tweets_df['text'])
+### Create list from filter object
 
-# # Create list from filter object result: res_list
-# res_list = list(result)
+# Select retweets from the Twitter DataFrame: result
+result = filter(lambda x: x[0:2] == 'RT', tweets_df['text'])
 
-# # Print all retweets in res_list
-# for tweet in res_list:
-#     print(tweet)
+# Create list from filter object result: res_list
+res_list = list(result)
+
+# Print all retweets in res_list
+for tweet in res_list:
+    print(tweet)
     
     
     
     
-"""
-DataCamp - Python Data Science Toolbox (Part 2)
-"""
+
+# =============================================================================
+# DataCamp - Python Data Science Toolbox (Part 2)
+# =============================================================================
+
 
 ### List comprehension
 
